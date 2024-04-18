83dea8f58791f458592aa00d085fdb91592f580f
Felipe Salomão <felipejbnicolau@hotmail.com>
2024-04-18 01:37:02 -0300

    adicionando as gems simple_form e kaminari

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,8 +4,8 @@ const webpack = require("webpack");
 environment.plugins.append(
   "Provide",
   new webpack.ProvidePlugin({
-    $: "jquery/src/jquery",
-    jQuery: "jquery/src/jquery",
+    $: "jquery",
+    jQuery: "jquery",
     Popper: ["popper.js", "default"],
   })
 );
